6321968b16bf35cb8b155f068374ebf0929e16a6
Anna Langner <annalangner06@gmail.com>
2020-02-26 13:02:53 +0100

    Hiding results and loading elements

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e) {
+  document.getElementById('results').style.display = 'none';
+  document.getElementById('loading').style.display = 'block';
+  setTimeout(calculateResults, 2000);
+  
+  e.preventDefault();
+});
 
-function calculateResults(e) {
+function calculateResults() {
   const amount = document.getElementById('amount');
   const interest = document.getElementById('interest');
   const years = document.getElementById('years');
@@ -20,14 +26,18 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculateNumberOfPayment).toFixed(2);
     totalInterest.value = ((monthly * calculateNumberOfPayment) - principal).toFixed(2);
+
+    document.getElementById('results').style.display = 'block';
+    document.getElementById('loading').style.display = 'none';
   } else {
     showError('Please check numbers');
   }
-
-  e.preventDefault();
 }
 
 function showError(error) {
+  document.getElementById('results').style.display = 'none';
+  document.getElementById('loading').style.display = 'none';
+
   const errorDiv = document.createElement('div');
   const card = document.querySelector('.card');
   const heading = document.querySelector('.heading');
